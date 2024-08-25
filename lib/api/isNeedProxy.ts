3c2de2dffd77cb87567bd5c01fa153b2677a743f
javier <nhat1234559@gmail.com>
2024-08-25 15:05:08 +0700

    fix(deploy): :ambulance: turn off proxy and use wss for socket endpoint

--- lib/api/isNeedProxy.ts
+++ lib/api/isNeedProxy.ts
@@ -5,8 +5,8 @@ import config from 'configs/app';
 // unsuccessfully tried different ways, even custom local dev domain
 // so for local development we have to use next.js api as proxy server
 export default function isNeedProxy() {
-  if (config.app.useProxy) {
-    return true;
+  if (!config.app.useProxy) {
+    return false;
   }
 
   return config.app.host === 'localhost' && config.app.host !== config.api.host;
